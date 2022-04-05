3c37c7c286114cbc7342e48b0d068703157153a4
Kinga Kohari <kinguca@yahoo.com>
2022-04-05 15:27:52 +0200

    Practiced query parameters

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,35 @@ app.get("/something", (req, res, next) => {
 app.get("/api/v1/users", (req, res, next) => {
     console.log("Request received for users endpoint.");
     res.sendFile(path.join(`${__dirname}/../frontend/users.json`));
+})
+
+app.get("/api/v1/users-query", (req, res) => {
+    console.dir(req.query)
+    console.dir(req.query.apiKey)
+    if (req.query.apiKey === "apple") {
+        res.sendFile(path.join(`${__dirname}/../frontend/users.json`))
+    } else {
+        res.send("Unauthorized request")
+    }
+})
+
+/* app.get("/api/v1/users-params/:key", (req, res) => {
+    console.dir(req.params)
+    console.log(req.params.key)
+    if (req.params.key === "apple") {
+        res.send("Azt írtad be, hogy alma")
+    } else {
+        res.send("Nem azt írtad be, hogy alma")
+    }   
+}) */
+
 
     /* const users = [...]  // tartalma átemelve a users.json-be 
     
     res.send(JSON.stringify(users)) */
-})
 
-app.get("/api/v1/users/active", (req, res, next) => {
+
+/* app.get("/api/v1/users/active", (req, res, next) => {
     console.log("Request received for active users endpoint.");
     fs.readFile("../frontend/users.json", (error, data) => {
         if (error) {
@@ -50,7 +72,25 @@ app.get("/api/v1/users/passive", (req, res, next) => {
             res.send(users.filter(user => user.status === "passive"));
         }
     })
+}) */
+
+app.get("/api/v1/users-params/:key", (req, res, next) => {
+    console.dir(req.params)
+    console.log(req.params.key)
+    fs.readFile("../frontend/users.json", (error, data) => {
+         if (req.params.key === "active") {
+            const users = JSON.parse(data)
+            const activeUsers = users.filter(user => user.status === "active");
+            res.send(activeUsers)
+        } else if (req.params.key === "passive"){
+            const users = JSON.parse(data)
+            const passiveUsers = users.filter(user => user.status === "passive");
+            res.send(passiveUsers)
+        } else res.send("An error occurred")
+    })
 })
+
+
 
 app.use('/pub', express.static(`${__dirname}/../frontend/public`));
 
